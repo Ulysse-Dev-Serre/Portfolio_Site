a6f72a29db736f480d8b41c75ab9c537e9ff9e17
Ulysse <ulyssebo255@gmail.com>
2025-07-12 11:11:03 -0400

    teste encore

--- src/teste/test.tsx
+++
@@ -1,9 +0,0 @@
-// src/components/Projects.tsx
-import React, { useState } from "react";
-import { ExternalLink, Github, Cpu, Leaf, Zap } from "lucide-react";
-import ProjectModal from "./ProjectModal"; // Importe le nouveau composant de modale
-
-const Projects: React.FC = () => {
-  // --- ÉTATS POUR LA MODALE ---
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedProject, setSelectedProject] = useState<any>(null); // Stockera le projet à afficher dans la modale
